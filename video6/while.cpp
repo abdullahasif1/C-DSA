b5ae27345aab35b82112137de5176b15481a8505
Abdullah Asif <asifabdullah63@yahoo.com>
2022-02-01 02:34:24 +0500

    While loop only

---
+++ video6/while.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+      int x;
+          cout<<"Enter the number from where you want to start counter till 0: ";
+          cin>>x;
+
+          while(x>0){
+
+              cout<<x<<endl;
+              x--;
+
+          }
+
+    return 0;
+}
