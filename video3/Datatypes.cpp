f8a927a3db61e959def299e33a94365ac0d07e33
Abdullah Asif <asifabdullah63@yahoo.com>
2022-01-31 14:29:04 +0500

    DataTypes and Size in memory

---
+++ video3/Datatypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+   int main(){
+      int a;
+      float b;
+      double c;
+      bool d;
+        
+        cout<<"Size of a(int) "<<sizeof(a)<<endl;
+        cout<<"Size of b(float) "<<sizeof(b)<<endl;
+        cout<<"Size of c(double) "<<sizeof(c)<<endl;
+        cout<<"Size of d(boolean) "<<sizeof(d)<<endl;
+      long int x;
+      short int y;
+
+        cout<<"Size of a(longint) "<<sizeof(x)<<endl;
+        cout<<"Size of a(shortint) "<<sizeof(y)<<endl;
+
+    return 0;
+
+}
