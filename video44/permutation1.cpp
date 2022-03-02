0a195a17b9f485a9564e230aaf9d90843be32e46
Abdullah Asif <asifabdullah63@yahoo.com>
2022-03-02 07:47:10 +0500

    Permutation of distinct integers through recursion

---
+++ video44/permutation1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include "bits/stdc++.h"
+using namespace std;
+
+vector<vector<int>> ans;
+
+void permute(vector<int> &a, int idx){
+    if(idx == a.size()){
+        ans.push_back(a);
+        return;
+    }
+    for(int i=idx;i<a.size();i++){
+        swap(a[i],a[idx]);
+        permute(a,idx+1);
+        swap(a[i],a[idx]);
+    }
+    return;
+}
+
+int main(){
+    int x;
+    cout<<"Enter the number of element: ";
+    cin>>x;
+
+    vector<int> a(x);
+    cout<<"Enter the value "<<x<<" number of values... "<<endl;
+    for(auto &i : a)
+        cin>>i;
+
+    permute(a,0);
+    for(auto v : ans){
+        for(auto i : v)
+            cout<<i<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
